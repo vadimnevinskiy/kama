c3bec1ac83e38c48cd46c64605310bf2adf15e74
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-02-01 15:10:52 +0500

    L#11 Main layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <div className="h-block">
+                    header
+                </div>
+            </header>
+            <nav className='nav'>
+                <a href="#">Profile</a>
+                <a  href="#">Messages</a>
+                <a  href="#">News</a>
+                <a  href="#">Music</a>
+                <a  href="#">Settings</a>
+            </nav>
+            <div className='content'>
+                <div className="avatar">
+                    <img src="https://f3.mylove.ru/J1NuDGy2QF.jpg"/>
+                </div>
+                <div className="mypost">
+                    <div className="post-item">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorem esse est explicabo labore maxime molestiae obcaecati quaerat sequi similique.
+                    </div>
+                    <div className="post-item">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi magni suscipit.
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
