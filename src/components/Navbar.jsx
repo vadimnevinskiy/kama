dd960b1b88ea1a8ca77dcc320da6bf6c3b130a61
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-02-02 00:13:11 +0500

    L#14 CSS: Added active menu item

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import classes from './Navbar.module.css';
 const Navbar = () => {
     return (
         <nav className={classes.navbar}>
-            <a href="#" className={classes.navbar__item}>Profile</a>
+            <a href="#" className={`${classes.navbar__item} ${classes.navbar__item_active}`}>Profile</a>
             <a href="#" className={classes.navbar__item}>Messages</a>
             <a href="#" className={classes.navbar__item}>News</a>
             <a href="#" className={classes.navbar__item}>Music</a>
