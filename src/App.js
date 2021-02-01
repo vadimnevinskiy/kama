8602a4a542b99465bcff13b334d433cb1618029d
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-02-01 15:30:43 +0500

    L#11 Main layout V0.2

--- src/App.js
+++ src/App.js
@@ -11,18 +11,28 @@ const App = () => {
             </header>
             <nav className='nav'>
                 <a href="#">Profile</a>
-                <a  href="#">Messages</a>
-                <a  href="#">News</a>
-                <a  href="#">Music</a>
-                <a  href="#">Settings</a>
+                <a href="#">Messages</a>
+                <a href="#">News</a>
+                <a href="#">Music</a>
+                <a href="#">Settings</a>
             </nav>
             <div className='content'>
-                <div className="avatar">
-                    <img src="https://f3.mylove.ru/J1NuDGy2QF.jpg"/>
+                <div className="user">
+                    <div className="user-avatar">
+                        <img src="https://f3.mylove.ru/J1NuDGy2QF.jpg"/>
+                    </div>
+                    <div className="user-name">
+                        <h3>MrBluff</h3>
+                        <div className="status">
+                            Заходил 6 минут назад
+                        </div>
+                    </div>
                 </div>
+
                 <div className="mypost">
                     <div className="post-item">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorem esse est explicabo labore maxime molestiae obcaecati quaerat sequi similique.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorem esse est explicabo
+                        labore maxime molestiae obcaecati quaerat sequi similique.
                     </div>
                     <div className="post-item">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi magni suscipit.
