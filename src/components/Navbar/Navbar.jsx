e5a3a084c34bcaf62bc575463b7da6dd93452f06
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-02-07 23:38:26 +0500

    L#20 NavLink: Added NavLink component to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.navbar}>
-            <a href="/profile" className={`${classes.navbar__item} ${classes.navbar__item_active}`}>
+            {/*<NavLink to="/profile" className={`${classes.navbar__item} ${classes.navbar__item_active}`}>*/}
+            {/*    <span className={`${classes.icon} material-icons`}>person</span>*/}
+            {/*    Profile*/}
+            {/*</NavLink>*/}
+            <NavLink to="/profile" className={classes.navbar__item} activeClassName={classes.active}>
                 <span className={`${classes.icon} material-icons`}>person</span>
                 Profile
-            </a>
-            <a href="/dialogs" className={classes.navbar__item}>
+            </NavLink>
+            <NavLink to="/dialogs" className={classes.navbar__item} activeClassName={classes.active}>
                 <span className={`${classes.icon} material-icons`}>mail</span>
                 Dialogs
-            </a>
-            <a href="/news" className={classes.navbar__item}>
+            </NavLink>
+            <NavLink to="/news" className={classes.navbar__item} activeClassName={classes.active}>
                 <span className={`${classes.icon} material-icons`}>event</span>
                 News
-            </a>
-            <a href="/music" className={classes.navbar__item}>
+            </NavLink>
+            <NavLink to="/music" className={classes.navbar__item} activeClassName={classes.active}>
                 <span className={`${classes.icon} material-icons`}>queue_music</span>
                 Music
-            </a>
-            <a href="/settings" className={classes.navbar__item}>
+            </NavLink>
+            <NavLink to="/settings" className={classes.navbar__item} activeClassName={classes.active}>
                 <span className={`${classes.icon} material-icons`}>settings</span>
                 Settings
-            </a>
+            </NavLink>
         </nav>
     );
 }
