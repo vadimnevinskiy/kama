4e17b3fcf2980ebd04012bddd2426215b8bcb04c
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-02-08 16:19:48 +0500

    L#21 Dialogs layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,19 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsList}>
+                <div className={classes.dialog + ' ' + classes.activeDialog}>Dima</div>
+                <div className={classes.dialog}>Vadim</div>
+                <div className={classes.dialog}>Katya</div>
+                <div className={classes.dialog}>Lena</div>
+                <div className={classes.dialog}>Viktor</div>
+            </div>
+            <div className={classes.messagesList}>
+                <div className={classes.message}>Lorem ipsum dolor sit amet.</div>
+                <div className={classes.message}>Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
+                <div className={classes.message}>Lorem ipsum dolor.</div>
+            </div>
         </div>
     )
 }
