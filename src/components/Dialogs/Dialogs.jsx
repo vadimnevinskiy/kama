0b7cde9e3216d951bc553b552179a42156e8210c
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-02-13 13:05:09 +0500

    L#23 Refactor Dialogs page: Move NavLink & Messages to components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,20 +2,36 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <NavLink className={classes.dialog} activeClassName={classes.activeDialog} to={path}>{props.name}</NavLink>
+    )
+}
+
+
+const MessageItem = (props) => {
+    return (
+        <div className={classes.message}>{props.message}</div>
+    )
+}
+
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsList}>
-                <NavLink className={classes.dialog + ' ' + classes.activeDialog} to="/dialogs/1">Dima</NavLink>
-                <NavLink className={classes.dialog} to="/dialogs/2">Vadim</NavLink>
-                <NavLink className={classes.dialog} to="/dialogs/3">Katya</NavLink>
-                <NavLink className={classes.dialog} to="/dialogs/4">Lena</NavLink>
-                <NavLink className={classes.dialog} to="/dialogs/5">Viktor</NavLink>
+                <DialogItem name="Dima" id="1" />
+                <DialogItem name="Vadim" id="2" />
+                <DialogItem name="Katya" id="3" />
+                <DialogItem name="Lena" id="4" />
+                <DialogItem name="Viktor" id="5" />
+                <DialogItem name="Sasha" id="6" />
+                <DialogItem name="Vasya" id="7" />
             </div>
             <div className={classes.messagesList}>
-                <div className={classes.message}>Lorem ipsum dolor sit amet.</div>
-                <div className={classes.message}>Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
-                <div className={classes.message}>Lorem ipsum dolor.</div>
+                <MessageItem message='Lorem ipsum dolor sit amet.'/>
+                <MessageItem message='Lorem ipsum dolor sit amet, consectetur adipisicing.'/>
+                <MessageItem message='Lorem ipsum dolor.'/>
             </div>
         </div>
     )
