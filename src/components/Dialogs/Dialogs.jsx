a103a84eaeb8787f1c33c78d518e9279a2bc8f76
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-02-13 13:34:03 +0500

    L#24 Move data to array

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,21 +17,34 @@ const MessageItem = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogs = [
+        {id: 0, name: 'Dima'},
+        {id: 1, name: 'Vadim'},
+        {id: 2, name: 'Katya'},
+        {id: 3, name: 'Lena'},
+        {id: 4, name: 'Viktor'},
+        {id: 5, name: 'Sasha'},
+        {id: 6, name: 'Vasya'},
+    ];
+
+    let messages = [
+        {id: 0, text: 'Lorem ipsum dolor sit amet.'},
+        {id: 0, text: 'Lorem ipsum dolor sit amet, consectetur adipisicing.'},
+        {id: 0, text: 'Lorem ipsum dolor.'}
+    ];
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsList}>
-                <DialogItem name="Dima" id="1" />
-                <DialogItem name="Vadim" id="2" />
-                <DialogItem name="Katya" id="3" />
-                <DialogItem name="Lena" id="4" />
-                <DialogItem name="Viktor" id="5" />
-                <DialogItem name="Sasha" id="6" />
-                <DialogItem name="Vasya" id="7" />
+                <DialogItem name={dialogs[0].name} id={dialogs[0].id} />
+                <DialogItem name={dialogs[1].name} id={dialogs[1].id} />
+                <DialogItem name={dialogs[2].name} id={dialogs[2].id} />
+                <DialogItem name={dialogs[3].name} id={dialogs[3].id} />
             </div>
             <div className={classes.messagesList}>
-                <MessageItem message='Lorem ipsum dolor sit amet.'/>
-                <MessageItem message='Lorem ipsum dolor sit amet, consectetur adipisicing.'/>
-                <MessageItem message='Lorem ipsum dolor.'/>
+                <MessageItem message={messages[0].text}/>
+                <MessageItem message={messages[1].text}/>
+                <MessageItem message={messages[2].text}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,12 +3,18 @@ import classes from './MyPosts.module.css';
 import Posts from "./Post/Posts";
 
 const MyPosts = () => {
+    let posts = [
+        {id: 0, text: 'Lorem ipsum dolor sit.', likes: 12},
+        {id: 1, text: 'Mecessitatibus numquam obcaecati officia porro quia quis vel.', likes: 1},
+        {id: 2, text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', likes: 2},
+        {id: 3, text: 'Consequatur deleniti libero nam.', likes: 334}
+    ];
     return (
         <div className={classes.posts}>
-            <Posts message='Lorem ipsum dolor sit.' likes='12' />
-            <Posts message='Mecessitatibus numquam obcaecati officia porro quia quis vel.' likes='1' />
-            <Posts message='Lorem ipsum dolor sit amet, consectetur adipisicing elit.' likes='2' />
-            <Posts message='Consequatur deleniti libero nam.' likes='999999' />
+            <Posts message={posts[0].text} likes={posts[0].likes} />
+            <Posts message={posts[1].text} likes={posts[1].likes} />
+            <Posts message={posts[2].text} likes={posts[2].likes} />
+            <Posts message={posts[3].text} likes={posts[3].likes} />
             <div className={classes.form}>
                 <div className={classes.fieldBox}>
                     <textarea className={classes.field}></textarea>
