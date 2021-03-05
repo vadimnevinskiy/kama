860848e327e3ae43155b87e44ec40a9448d53142
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-03-06 01:53:57 +0500

    L#59 MINOR: added target blank for links

--- src/components/Profile/UserInfo/UserInfo.jsx
+++ src/components/Profile/UserInfo/UserInfo.jsx
@@ -32,13 +32,13 @@ const UserInfo = (props) => {
                 </div>
                 <div className={classes.user__contacts}>
                     <div className={classes.user__status}>
-                        <div><a href={`http://${props.profile.contacts.facebook}`} >{props.profile.contacts.facebook}</a></div>
-                        <div><a href={`http://${props.profile.contacts.website}`}>{props.profile.contacts.website}</a></div>
-                        <div><a href={`http://${props.profile.contacts.vk}`}>{props.profile.contacts.vk}</a></div>
-                        <div><a href={`http://${props.profile.contacts.twitter}`}>{props.profile.contacts.twitter}</a></div>
-                        <div><a href={`http://${props.profile.contacts.youtube}`}>{props.profile.contacts.youtube}</a></div>
-                        <div><a href={`http://${props.profile.contacts.github}`}>{props.profile.contacts.github}</a></div>
-                        <div><a href={`http://${props.profile.contacts.mainLink}`}>{props.profile.contacts.mainLink}</a></div>
+                        <div><a href={`http://${props.profile.contacts.facebook}`} target="_blank">{props.profile.contacts.facebook}</a></div>
+                        <div><a href={`http://${props.profile.contacts.website}`} target="_blank">{props.profile.contacts.website}</a></div>
+                        <div><a href={`http://${props.profile.contacts.vk}`} target="_blank">{props.profile.contacts.vk}</a></div>
+                        <div><a href={`http://${props.profile.contacts.twitter}`} target="_blank">{props.profile.contacts.twitter}</a></div>
+                        <div><a href={`http://${props.profile.contacts.youtube}`} target="_blank">{props.profile.contacts.youtube}</a></div>
+                        <div><a href={`http://${props.profile.contacts.github}`} target="_blank">{props.profile.contacts.github}</a></div>
+                        <div><a href={`http://${props.profile.contacts.mainLink}`} target="_blank">{props.profile.contacts.mainLink}</a></div>
                     </div>
                 </div>
             </div>
