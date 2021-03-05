7d32fc9007e40995e06187b532c9320887f694b2
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-03-05 16:42:30 +0500

    L#56 Refactoring: Move class UserAPIComponent from UsersApiComponent.jsx and rename it as UsersContainer

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
-import UsersAPIComponent from "./UsersAPIComponent";
 import {connect} from "react-redux";
 import {followAC, setUsersAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC} from "../../redux/users-reducer";
+import * as axios from "axios";
+import Users from "./Users";
+
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(responce => {
+                this.props.setUsers(responce.data.items);
+                this.props.setTotalUsersCount(responce.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(responce => {
+                this.props.setUsers(responce.data.items)
+            });
+    }
+
+    render() {
+        return (
+            <Users
+                totalUsersCount = {this.props.totalUsersCount}
+                pageSize = {this.props.pageSize}
+                currentPage = {this.props.currentPage}
+                onPageChanged = {this.onPageChanged}
+                users = {this.props.users}
+                follow = {this.props.follow}
+                unfollow = {this.props.unfollow}
+            />
+        )
+    }
+}
+
 
 let mapStateToProps = (state) => {
     return {
@@ -31,4 +66,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
