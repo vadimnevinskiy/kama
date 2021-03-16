b3cd7554f1e1cc9909bdb38d70f75afa4ae29359
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-03-17 00:47:28 +0500

    L#73 Updated react-scripts to 4.0.3 'npm install --save --save-exact react-scripts@4.0.3'

--- src/components/Profile/UserInfo/UserStatus.jsx
+++ src/components/Profile/UserInfo/UserStatus.jsx
@@ -3,22 +3,22 @@ import classes from './UserStatus.module.css';
 
 class UserStatus extends React.Component {
     state = {
-        editMode: true
+        editMode: false
     }
 
-    activateEditMode() {
+    activateEditMode = () => {
         this.setState({
             editMode: true
         })
     }
-    deActivateEditMode() {
+    deActivateEditMode = () => {
         this.setState({
             editMode: false
         })
     }
 
     onChangeStatus(value) {
-       
+
     }
 
     render() {
@@ -27,12 +27,12 @@ class UserStatus extends React.Component {
                 {!this.state.editMode &&
                     <div className={classes.statusBox}>
                         <span className={classes.status}>{this.props.status}</span>
-                        <span onClick={ this.activateEditMode.bind(this) } className={`${classes.userIcon} material-icons`}>border_color</span>
+                        <span onClick={ this.activateEditMode } className={`${classes.userIcon} material-icons`}>border_color</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div className={classes.statusBox}>
-                        <input autoFocus={true} onBlur={ this.deActivateEditMode.bind(this) } className={classes.statusField} type="text" value={this.props.status} onChange={this.onChangeStatus}/>
+                        <input autoFocus={true} onBlur={ this.deActivateEditMode } className={classes.statusField} type="text" value={this.props.status} onChange={this.onChangeStatus}/>
                         <span className={`${classes.userIcon} ${classes.saveIcon} material-icons`}>save</span>
                     </div>
                 }
