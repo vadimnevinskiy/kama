708bdc590bc2ee69ffb9e822f40223a843daac8b
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-03-29 23:14:52 +0500

    L#81 added users-selectors.js

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -4,7 +4,7 @@ import {
     follow,
     unfollow,
     toggleFollowingProgress,
-    getUsers,
+    requestUsers,
     pageChanged
 } from '../../redux/users-reducer';
 import Users from './Users';
@@ -12,11 +12,19 @@ import Preloader from '../common/preloader/Preloader';
 import {Redirect} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
+import {
+    getUsers,
+    getCurrentPage,
+    getFollowingInProgress,
+    getIsFetching,
+    getPageSize,
+    getTotalUsersCount
+} from "../../redux/users-selectors";
 
 
 class UsersContainer extends React.Component {
     componentDidMount() {
-        this.props.getUsers(this.props.currentPage, this.props.pageSize);
+        this.props.requestUsers(this.props.currentPage, this.props.pageSize);
     }
 
     onPageChanged = (pageNumber) => {
@@ -49,26 +57,38 @@ class UsersContainer extends React.Component {
 }
 
 
+// let mapStateToProps = (state) => {
+//     return {
+//         users: state.usersPage.users,
+//         pageSize: state.usersPage.pageSize,
+//         totalUsersCount: state.usersPage.totalUsersCount,
+//         currentPage: state.usersPage.currentPage,
+//         isFetching: state.usersPage.isFetching,
+//         followingInProgress: state.usersPage.followingInProgress,
+//         isAuth: state.auth.isAuth
+//     }
+// }
+
+
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress,
+        users: getUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state),
         isAuth: state.auth.isAuth
     }
 }
 
 
-
 export default compose(
     connect(mapStateToProps, {
-        follow,
-        unfollow,
-        toggleFollowingProgress,
-        getUsers: getUsers,
+        follow: follow,
+        unfollow: unfollow,
+        toggleFollowingProgress: toggleFollowingProgress,
+        requestUsers: requestUsers,
         pageChanged: pageChanged
     }),
     // withAuthRedirect
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -108,7 +108,7 @@ export const toggleFollowingProgress = (isFetching, userId) => {
 
 
 // THUNK CREATORS
-export const getUsers = (currentPage, pageSize) => {
+export const requestUsers = (currentPage, pageSize) => {
     return (dispatch) => {
         dispatch(toggleIsFetching(true));
         usersAPI.getUsers(currentPage, pageSize)
