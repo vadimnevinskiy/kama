9e5e7ed72867a61a8306d7a6bdfb0265eaa2c39b
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-04-10 14:55:19 +0500

    L#90 REFACTORING: Profile - destructuring props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import classes from './Profile.module.css';
-import MyPosts from './MyPosts/MyPosts';
 import UserInfo from './UserInfo/UserInfo';
 import MyPostsContainer from './MyPosts/MyPostsContainer';
 
-const Profile = (props) => {
+const Profile = ({profile, status, updateStatus}) => {
     return (
         <div className={classes.profile}>
             <UserInfo
-                profile={props.profile}
-                status={props.status}
-                updateStatus={props.updateStatus}
+                profile={profile}
+                status={status}
+                updateStatus={updateStatus}
             />
             <MyPostsContainer />
-            {/*<MyPosts profile={props.profile} dispatch={props.dispatch} />*/}
         </div>
     );
 }
