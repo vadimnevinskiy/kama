f8fa2a79576c940caeab236f9711a8493e625cec
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-06-13 02:23:49 +0500

    Installed @types/classnames & Paginator.jsx refactoring to .tsx

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,21 +1,30 @@
-import React, {useState} from "react";
-import classes from "./Paginator.module.css";
-import classNames from "classnames";
+import React, {useState} from 'react'
+import classes from './Paginator.module.css'
+import classNames from 'classnames'
 
-let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
-    let pagesCount = Math.ceil(totalItemsCount / pageSize);
-    let pages = [];
+
+type PropsType = {
+    totalItemsCount: number
+    pageSize: number
+    currentPage: number
+    onPageChanged: (pageNumber: number) => void
+    portionSize: number
+}
+
+let Paginator: React.FC<PropsType> = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+    let pagesCount = Math.ceil(totalItemsCount / pageSize)
+    let pages: Array<number> = []
     for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
+        pages.push(i)
         // if (pagesCount > i && i > (pagesCount - 10)) {
         //     pages.push(i);
         // }
     }
 
-    let portionCount = Math.ceil(pagesCount / portionSize);
+    let portionCount = Math.ceil(pagesCount / portionSize)
     let [portionNumber, setPortionNumber] = useState(1)
-    let leftPortionNumber = (portionNumber - 1) * portionSize + 1;
-    let rightPortionNumber = portionNumber * portionSize;
+    let leftPortionNumber = (portionNumber - 1) * portionSize + 1
+    let rightPortionNumber = portionNumber * portionSize
 
     return (
         <div className={classes.paginator}>
@@ -73,4 +82,4 @@ let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portion
     )
 }
 
-export default Paginator;
+export default Paginator
